81cf61733b7e49375246fe1cfaf5761c9882d874
Fiona Wilson <fionaw237@gmail.com>
2024-02-01 14:51:30 +0000

    Create the FeedItem model

---
+++ EssentialDeveloperFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialDeveloperFeed
+//
+//  Created by Fiona Wilson on 01/02/2024.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
