db56bbd8d66f442004adc4b38c09f9d032f112ef
Fiona Wilson <fiona.wilson@resdiary.com>
2024-02-01 14:43:22 +0000

    Initial Commit

---
+++ EssentialDeveloperFeed/EssentialDeveloperFeed.h
@@ -0,0 +1,18 @@
+//
+//  EssentialDeveloperFeed.h
+//  EssentialDeveloperFeed
+//
+//  Created by Fiona Wilson on 01/02/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialDeveloperFeed.
+FOUNDATION_EXPORT double EssentialDeveloperFeedVersionNumber;
+
+//! Project version string for EssentialDeveloperFeed.
+FOUNDATION_EXPORT const unsigned char EssentialDeveloperFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialDeveloperFeed/PublicHeader.h>
+
+
