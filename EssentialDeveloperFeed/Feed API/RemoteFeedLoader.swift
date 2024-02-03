f14f05ae26573b90df192d30145e47a593a48b33
Fiona Wilson <fionaw237@gmail.com>
2024-02-03 16:56:04 +0000

    Delivers items array on 200 HTTP response with JSON items

--- EssentialDeveloperFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialDeveloperFeed/Feed API/RemoteFeedLoader.swift
@@ -39,9 +39,9 @@ public final class RemoteFeedLoader {
     public func load(completion: @escaping  (Result) -> Void) {
         client.get(from: url) { result in
             switch result {
-            case let .success(data, response):
-                if let _ = try? JSONSerialization.jsonObject(with: data) {
-                    completion(.success([]))
+            case let .success(data, _):
+                if let root = try? JSONDecoder().decode(Root.self, from: data) {
+                    completion(.success(root.items))
                 } else {
                     completion(.failure(.invalidData))
                 }
@@ -50,4 +50,8 @@ public final class RemoteFeedLoader {
             }
         }
     }
+}
+
+private struct Root: Decodable {
+    let items: [FeedItem]
 }
