f6cfebd5ecb25ecc23dafd9e53d8b9b7a5ecc18b
Fiona Wilson <fionaw237@gmail.com>
2024-03-29 16:46:53 +0000

    Move LocalFeedItem to its own file

---
+++ EssentialDeveloperFeed/EssentialDeveloperFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,22 @@
+//
+//  LocalFeedItem.swift
+//  EssentialDeveloperFeed
+//
+//  Created by Fiona Wilson on 29/03/2024.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
